e6c325c9ebb7b7302fbff99b3539130823a25bbe
Kulbhushan27 <92461660+Kulbhushan27@users.noreply.github.com>
2021-11-20 11:57:57 +0530

    Add files via upload

---
+++ ProjectSecond/src/Second.java
@@ -0,0 +1,13 @@
+
+public class Second {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("First line");
+		System.out.println("Second Line");
+		System.out.println("Third line");
+		
+	}
+
+}
